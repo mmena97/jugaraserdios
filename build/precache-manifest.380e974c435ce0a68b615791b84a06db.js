a67bd6008924033cd682c93d7193ae1b48fb79b9
Miguel Angel <mena_alcalde@hotmail.com>
2020-03-08 13:55:50 +0100

    dios

---
+++ build/precache-manifest.380e974c435ce0a68b615791b84a06db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161f3f9a5c0a85acfe9fcd76ae786496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11fc7a0bb05f67cf6607",
+    "url": "/static/css/2.a00eef2d.chunk.css"
+  },
+  {
+    "revision": "8c6ce3d778a8f631d93b",
+    "url": "/static/css/main.7cd73978.chunk.css"
+  },
+  {
+    "revision": "11fc7a0bb05f67cf6607",
+    "url": "/static/js/2.5e3f45bf.chunk.js"
+  },
+  {
+    "revision": "edfc1f2bdfcad1767fef8b6ee0f470c5",
+    "url": "/static/js/2.5e3f45bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6ce3d778a8f631d93b",
+    "url": "/static/js/main.ae3489b6.chunk.js"
+  },
+  {
+    "revision": "e92c818309134516c215",
+    "url": "/static/js/runtime-main.25db6db8.js"
+  }
+]);
